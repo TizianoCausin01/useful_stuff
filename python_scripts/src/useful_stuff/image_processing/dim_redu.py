c0cc12bfda5bcc29ed44cec18b08e198f200131c
TizianoCausin01 <causintiziano@gmail.com>
2026-06-03 19:11:43 +0200

    Added possibility to sub-batch (improved)

--- python_scripts/src/useful_stuff/image_processing/dim_redu.py
+++ python_scripts/src/useful_stuff/image_processing/dim_redu.py
@@ -17,34 +17,38 @@ INPUT:
 OUTPUT:
     - ipcas: dict[str, IncrementalPCA] -> fitted IncrementalPCA objects for each layer
 """
-def compute_img_ipca(ann: imgANN, loader: DataLoader, ipcas: dict[str: IncrementalPCA], device: torch, rank=0, sub_batches=None):
+def compute_img_ipca(ann: imgANN, loader: DataLoader, ipcas: dict[str: IncrementalPCA], device: torch, rank=0, sub_batch_size=None):
     if sub_batch_size is None:
-        sub_batch_size = loader_batch_size
+        sub_batch_size = loader.batch_size
 
-    features_list = {layer: [] for layer in ann.handles.keys()}
     with torch.no_grad():
         # Forward each image batch and let imgANN hooks collect target-layer features.
         for idx, (images, _) in enumerate(loader):
             st_forw = time.time()
-            for i in range(0, batch_size, sub_batch_size):
-                sub_batch = batch[i:i+sub_batch_size]
+            features_list = {layer: [] for layer in ann.handles.keys()}
+            for i in range(0, loader.batch_size, sub_batch_size):
+                sub_batch = images[i:i+sub_batch_size]
                 sub_batch = sub_batch.to(device)
                 ann.model(sub_batch)
-                for layer, features in ann.features.items():
+                del sub_batch
+                for layer_name, features in ann.features.items():
                     f = features.detach().cpu().numpy()
-                    features_list[layer].append(f)
-                    ANN.features[layer_name] = None
-                    torch.cuda.empty_cache() 
-                        
+                    features_list[layer_name].append(f)
+                    ann.features[layer_name] = None
+                    print_wise(f"sub_batch shape {f.shape}", rank=rank)
+                    if torch.cuda.is_available():
+                        torch.cuda.empty_cache() 
             end_forw = time.time()
             print_wise(f"forward took {end_forw - st_forw}", rank=rank)
             # Update each layer's iPCA with the current batch of activations.
-            for layer in ann.features.keys():
+            for layer_name in ann.features.keys():
                 st_ipca = time.time()
-                ipcas[layer].partial_fit(features_list[layer_name])
+                f_full = np.concatenate(features_list[layer_name], axis=0)
+                print_wise(f"total batch shape {f_full.shape}", rank=rank)
+                ipcas[layer_name].partial_fit(f_full)
                 features_list[layer_name] = []
                 end_ipca = time.time()
-                print_wise(f"ipca fit {layer} took {end_ipca - st_ipca}", rank=rank)
+                print_wise(f"ipca fit {layer_name} took {end_ipca - st_ipca}", rank=rank)
             print_wise(f"Computed batch {idx}/{len(loader)-1} of {ann.model_name}: {list(ipcas.keys())}", rank=rank)
     return ipcas
 # EOF 
